37253d18a6777225017f5a167a99989c2c0016ff
ian knauer <ian.knauer@gmail.com>
2019-02-26 08:42:23 -0800

    update version & router pointing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DavosCharityApi.MixProject do
   def project do
     [
       app: :davos_charity_api,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
