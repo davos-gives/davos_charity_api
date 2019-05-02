b2708ea7e3c5525789b2c2f013fb2c7e92e04fd8
ian knauer <ian.knauer@gmail.com>
2019-05-01 20:49:07 -0700

    add expiry to vault card view

--- lib/davos_charity_api_web/views/vault_card_view.ex
+++ lib/davos_charity_api_web/views/vault_card_view.ex
@@ -3,5 +3,5 @@ defmodule DavosCharityApiWeb.VaultCardView do
   use JaSerializer.PhoenixView
 
   location "/api/vault-cards/:id"
-  attributes [:iats_id, :name, :card_type, :last_four_digits, :primary]
+  attributes [:iats_id, :name, :card_type, :last_four_digits, :primary, :expiry_year, :expiry_month]
 end
