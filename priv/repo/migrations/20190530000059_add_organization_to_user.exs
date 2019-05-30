462b98c8b50b6a905bd4754c38445b7264435b42
ian knauer <ian.knauer@gmail.com>
2019-05-29 20:30:56 -0700

    commit migration

---
+++ priv/repo/migrations/20190530000059_add_organization_to_user.exs
@@ -0,0 +1,10 @@
+defmodule DavosCharityApi.Repo.Migrations.AddOrganizationToUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :organization_id, references(:organizations)
+    end
+
+  end
+end
