72a9fcdb1921699dd9795bda3cc711b54f00bd1d
ian knauer <ian.knauer@gmail.com>
2019-06-10 11:11:32 -0700

    remove duplicate migration

--- priv/repo/migrations/20181115193221_add_password_fields_to_donor.exs
+++
@@ -1,9 +0,0 @@
-defmodule DavosCharityApi.Repo.Migrations.AddPasswordFieldsToDonor do
-  use Ecto.Migration
-
-  def change do
-    alter table(:donors) do
-      add :password_hash, :string
-    end
-  end
-end
